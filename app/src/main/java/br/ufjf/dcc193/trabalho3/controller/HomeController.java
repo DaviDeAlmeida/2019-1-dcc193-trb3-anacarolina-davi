f07adaf52a9f36896b5bf8a42e313d3683f2f908
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-06-27 14:38:09 -0300

    Criação do layout e da página home

---
+++ app/src/main/java/br/ufjf/dcc193/trabalho3/controller/HomeController.java
@@ -0,0 +1,19 @@
+
+package br.ufjf.dcc193.trabalho3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author acaro
+ */
+@Controller
+public class HomeController {
+    
+    @RequestMapping({"", "/", "home"})
+    public String index() {
+        return "index";
+    }
+    
+}
