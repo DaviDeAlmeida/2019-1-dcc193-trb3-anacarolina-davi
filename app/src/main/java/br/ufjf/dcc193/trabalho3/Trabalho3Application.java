2eba4a776fd88d762f5ad15108d9e4c04bc8fde2
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-06-27 14:18:32 -0300

    Criação do projeto

---
+++ app/src/main/java/br/ufjf/dcc193/trabalho3/Trabalho3Application.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trabalho3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho3Application.class, args);
+	}
+
+}
