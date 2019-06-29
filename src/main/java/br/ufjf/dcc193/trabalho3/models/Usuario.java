ca35f1454ee905eb9e503beb9c6c727e84e0c9db
davicardoso <davi.almeida.c@gmail.com>
2019-06-29 19:12:22 -0300

    Model Usuario adicionado

---
+++ src/main/java/br/ufjf/dcc193/trabalho3/models/Usuario.java
@@ -0,0 +1,61 @@
+package br.ufjf.dcc193.trabalho3.models;
+
+public class Usuario {
+
+    private long idUsuario;
+    private String nomeCompleto;
+    private String codAcesso;
+    private String descricao;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(long idUsuario, String nomeCompleto, String codAcesso, String descricao, String email) {
+        this.idUsuario = idUsuario;
+        this.nomeCompleto = nomeCompleto;
+        this.codAcesso = codAcesso;
+        this.descricao = descricao;
+        this.email = email;
+    }
+
+    public long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCodAcesso() {
+        return codAcesso;
+    }
+
+    public void setCodAcesso(String codAcesso) {
+        this.codAcesso = codAcesso;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
