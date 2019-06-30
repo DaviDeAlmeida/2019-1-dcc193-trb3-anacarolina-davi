4c7a99d470f859cf6adeac9ef63e037845a02d60
davicardoso <davi.almeida.c@gmail.com>
2019-06-29 21:29:26 -0300

    VinculoRepository iniciado

---
+++ src/main/java/br/ufjf/dcc193/trabalho3/Repository/VinculoRepository.java
@@ -0,0 +1,7 @@
+package br.ufjf.dcc193.trabalho3.Repository;
+
+import br.ufjf.dcc193.trabalho3.models.Vinculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VinculoRepository extends JpaRepository<Vinculo, Long> {
+}
